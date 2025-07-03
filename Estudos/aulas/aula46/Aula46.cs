3551865c0d585ff802bebfe8175b03771b42a280
MurilloMorgado <mumorgado@hotmail.com>
2025-07-03 11:54:42 -0300

    convertendo para metodo static

--- Estudos/aulas/aula46/Aula46.cs
+++ Estudos/aulas/aula46/Aula46.cs
@@ -5,29 +5,20 @@ namespace Estudos.aulas.aula46
 {
     internal class Aula46
     {
-        static double pi = 3.14;
         public void CalcularCircunferencia()
         {
             Console.Write("Informe o valor do raio:");
             double raio= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            string circ = circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture);
-            string vol = Volume(raio).ToString("F2", CultureInfo.InvariantCulture);
+            string circ = Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture);
+            string vol = Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture);
 
             Console.WriteLine("Circulo: " + circ);
             Console.WriteLine("Volume: " + vol);
-            Console.WriteLine("Valor de PI: " + pi);
+            Console.WriteLine("Valor de PI: " + Calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
         }
         
-        static double circunferencia(double raio)
-        {
-            return 2.0 * pi * raio;
-        }
 
-        static double Volume(double raio)
-        {
-            return ((4.0 / 3.0) * pi * Math.Pow(raio, 3.0));
-        }
 
     }
 }
